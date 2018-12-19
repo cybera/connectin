ec076c3c06a3e79737dac8b7e5e9350b38e2ea03
Tatiana Meleshko <tatiana.meleshko@gmail.com>
2018-12-19 22:00:47 +0000

    changed shell script and absolute paths

--- migration_scripts/load_database.sh
+++ migration_scripts/load_database.sh
@@ -1,6 +1,13 @@
 #!/bin/sh
-for i in `seq 1 1000`;
+if [ -z "$1" ]
+  then
+    num_iterations=1000
+  else
+    num_iterations=$1
+fi
+
+for i in `seq 1 $num_iterations`;
 do
-        echo "--- Iteration #$i:---"
-	python3 mssql_to_influx.py
+        echo "--- Iteration #$i out of $num_iterations:---"
+	python3 /root/connectin/migration_scripts/mssql_to_influx.py
 done
